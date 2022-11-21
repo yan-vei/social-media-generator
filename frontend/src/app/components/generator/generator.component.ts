fd59c3dd8ad4a1ba53cbdd42ce3410c903cff914
Yana Veitsman <veytsman.karaganda@gmail.com>
2022-11-21 14:21:22 +0200

    basic tweet generation

--- frontend/src/app/components/generator/generator.component.ts
+++ frontend/src/app/components/generator/generator.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { RequestDataUrl } from 'src/app/entities/request-data.model';
+import { GeneratorService } from 'src/app/services/generator.service';
 
 @Component({
   selector: 'generator',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./generator.component.css']
 })
 export class GeneratorComponent implements OnInit {
+  urlForm = this.fb.group({
+    url: [, Validators.required]
+  });
 
-  constructor() { }
+  constructor(private generatorService: GeneratorService, private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
 
+  generateTweets()
+  {
+    let data = new RequestDataUrl(this.urlForm.controls.url.value || '')
+    this.generatorService.generateTweetsFromUrl(data)
+    .subscribe((tweets) =>
+    console.log(tweets)
+    )
+  }
 }
--- frontend/src/app/services/article-api.service.ts
+++ frontend/src/app/services/article-api.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpErrorResponse} from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 import {API_URL} from '../env';
 import {Article} from '../entities/article.model';
