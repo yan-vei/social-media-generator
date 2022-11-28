941366f0dca3343369eabd6d0376b1ac56185b12
Yana Veitsman <veytsman.karaganda@gmail.com>
2022-11-28 22:06:41 +0200

    basic styling for home component

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { LogoutService } from 'src/app/services/logout.service';
 
 @Component({
   selector: 'home',
@@ -8,7 +9,7 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, public logout: LogoutService) { }
 
   ngOnInit(): void {
     if (localStorage.getItem('user_token'))
