2ef42d52f6b5f1835c1a5fd8411cdb0962f20d9b
Yana Veitsman <veytsman.karaganda@gmail.com>
2022-11-29 22:55:25 +0200

    check file extension

--- backend/src/routes/admin_routes.py
+++ backend/src/routes/admin_routes.py
@@ -15,11 +15,15 @@ def process_configs():
             path = cwd + SLASH + "data" + SLASH + filename
             return send_file(path, as_attachment=True)
         except FileNotFoundError:
-            return make_response(jsonify({"message": "File not found"}), 404)
+            return make_response(jsonify({"message": "File not found."}), 404)
 
     elif request.method == 'POST':
         try:
             file = request.files["file"]
+
+            if os.path.splitext(file.filename)[1] != '.txt' and os.path.splitext(file.filename)[1] != '.json':
+                return make_response(jsonify({"error": "Invalid file format."}), 400)
+
             file.save(os.path.join(cwd + SLASH + 'data' + SLASH, file.filename))
             return make_response(jsonify({"message": "Settings have been updated."}), 200)
         except Exception as e:
